2d0b5fea0232db18431a3a37ca6b8c1b6515fcc3
Busiedblowfish <oobey1@aol.com>
2016-11-23 00:44:57 -0500

    Added User.aspx with verified identity

---
+++ Chatta/Account/User.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="Chatta: Chat Hub" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="User.aspx.cs" Inherits="Chatta.Account.User" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Chatta/Account/User.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+
+namespace Chatta.Account
+{
+    public partial class User : System.Web.UI.Page
+    {
+        //Redirect User to Login page if they are not logged in
+        private void RedirectOnFail()
+        {
+            Response.Redirect((User.Identity.IsAuthenticated) ? "~/Account/User" : "~/Account/Login");
+        }
+
+        bool isLoggedIn = System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!isLoggedIn)
+            {
+                RedirectOnFail();
+                return;
+            }
+        }
+    }
+}
